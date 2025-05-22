94a4151ffd9e822ab0a301fb930c918ba6faaba8
Edward <edw.chydi@gmail.com>
2025-05-22 19:35:31 +0900

    fourth commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Narrivals from "@/components/Narrivals";
